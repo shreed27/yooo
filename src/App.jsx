f977775daca7e6fee12cb82b2009f67a74baeb73
BobTheBuilder <136893148+shreed27@users.noreply.github.com>
2024-12-06 02:38:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
              rel="noopener noreferrer"
              className="block">
             <h1 className="text-4xl font-bold text-blue-500 hover:text-blue-600 transition-colors">
-              MoneyMiner
+              MoneyMiner.AI
             </h1>
           </a>
         </header>
